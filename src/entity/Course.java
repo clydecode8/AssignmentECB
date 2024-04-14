3f8140e7475f8cf07e5b07725e201ce36aab3f94
Admin <Admin@LAPTOP-MQ0LS5OM.mshome.net>
2024-04-14 14:45:30 +0800

    wss newww bij

--- src/entity/Course.java
+++ src/entity/Course.java
@@ -1,5 +1,8 @@
-
 package entity;
+
+import adt.ArrayList;
+import adt.LinkedList;
+import adt.ListInterface;
 
 /**
  *
@@ -9,6 +12,58 @@ public class Course {
     private String courseName;
     private String courseID;
     private String courseDetails;
+    private String courseType;
+    //private final static ListInterface<Course> courseList = new ArrayList<>();
+    private LinkedList<Student> studentList = new LinkedList<>();
     
+    public Course(){
+        
+    }
+    
+    public Course(String courseName, String courseID, String courseDetails) {
+        this.courseName = courseName;
+        this.courseID = courseID;
+        this.courseDetails = courseDetails;
+    }
+
+    public String getCourseName() {
+        return courseName;
+    }
+
+    public String getCourseID() {   
+        return courseID;
+    }
+
+    public String getCourseDetails() {
+        return courseDetails;
+    }
+    
+    public String getcourseType(){
+        return courseType;
+    }
+
+    public LinkedList<Student> getStudentList() {
+        return studentList;
+    }
+
+    public void setCourseName(String courseName) {
+        this.courseName = courseName;
+    }
+
+    public void setCourseID(String courseID) {
+        this.courseID = courseID;
+    }
+
+    public void setCourseDetails(String courseDetails) {
+        this.courseDetails = courseDetails;
+    }
+    
+    public void getCourseType(String courseType) {
+        this.courseType = courseType;
+    }
+
+    public void setStudentList(LinkedList<Student> studentList) {
+        this.studentList = studentList;
+    }
     
 }
